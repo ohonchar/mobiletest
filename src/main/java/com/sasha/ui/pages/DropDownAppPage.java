80c4c33d0e3a5f39f387964a7bd03181681701e4
aleksandrhonchar <aleksandr.honchar@adsquare.com>
2023-03-26 14:42:02 +0200

    First test with PageFactory implemented

--- src/main/java/com/sasha/ui/pages/DropDownAppPage.java
+++ src/main/java/com/sasha/ui/pages/DropDownAppPage.java
@@ -2,11 +2,30 @@ package com.sasha.ui.pages;
 
 import com.sasha.ui.BasePage;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class DropDownAppPage extends BasePage {
+    public DropDownAppPage() {
+        super();
+    }
+    private final String spinnerLocator = "//android.widget.Spinner[@resource-id='com.jaimin.spinner:id/spinner1']";
+
+    @FindBy(xpath = spinnerLocator)
+    private WebElement dropDown;
+
+    @FindBy(xpath = spinnerLocator + extendedTextLocator)
+    private WebElement dropDownText;
 
     public DropDownAppPage clickOnDropDownMenu() {
-        findElement(AppiumBy.xpath("//android.widget.Spinner[@resource-id='com.jaimin.spinner:id/spinner1']")).click();
+        dropDown.click();
+        log.info("[STEP]::Click on dropdown menu");
         return this;
     }
+
+    public String getDropDownText() {
+        log.info("[STEP]::Retrieve DropDown text");
+        return dropDownText.getText();
+    }
+
 }
