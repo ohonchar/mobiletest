53d954800ba5851c3b7bc0bf11dc4f50fc69d036
aleksandrhonchar <aleksandr.honchar@adsquare.com>
2023-03-27 21:22:13 +0200

    Add restart app method

--- src/main/java/com/sasha/utils/ui/UiHelpers.java
+++ src/main/java/com/sasha/utils/ui/UiHelpers.java
@@ -1,6 +1,8 @@
 package com.sasha.utils.ui;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,27 +14,37 @@ import java.time.Duration;
 
 public class UiHelpers {
     private final WebDriver driver = LocalDriverManager.getMobileDriver();
-    public WebElement scrollTo(String visibleText) {
+    protected WebElement scrollTo(String visibleText) {
         return findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(" +
                 "new UiSelector().scrollable(true).instance(0))" +
                 ".scrollIntoView(new UiSelector().textContains(\""+visibleText+"\").instance(0))"));
     }
 
-    public WebElement findElement(By element) {
+    protected WebElement findElement(By element) {
         return findElement(element, 5);
     }
-    public WebElement findElement(By element, int defaultTimeout) {
+    protected WebElement findElement(By element, int defaultTimeout) {
         return new WebDriverWait(driver, Duration.ofSeconds(defaultTimeout))
                 .until(ExpectedConditions.visibilityOfElementLocated(element));
     }
 
-    public Alert waitUntilAlertIsPresent() {
-        return new WebDriverWait(driver, Duration.ofSeconds(5))
+    protected Alert waitUntilAlertIsPresent(long timeToWait) {
+        return new WebDriverWait(driver, Duration.ofSeconds(timeToWait))
                 .until(ExpectedConditions.alertIsPresent());
     }
 
-    public <T> T goBack(T type) {
+    public  <T> T goBack(T type) {
         driver.navigate().back();
         return type;
+    }
+
+    public void restartAndroidApp() {
+        ((AndroidDriver) driver).terminateApp("io.appium.android.apis");
+        ((AndroidDriver) driver).activateApp("io.appium.android.apis");
+    }
+
+    public void restartIosApp() {
+        ((IOSDriver) driver).terminateApp("com.apple.Preferencess");
+        ((IOSDriver) driver).activateApp("com.apple.Preferences");
     }
 }
